c9e28313ef69af4ec55d59738796c4feec102aa3
Kevin Shook <kevin.shook@usask.ca>
2017-10-11 21:08:26 -0600

    First commit

---
+++ R/hydRology-package.R
@@ -0,0 +1,10 @@
+#' @title Functions for Canadian hydrological analysis
+#' @docType package
+#' @name hydRology-package
+#' 
+#' @description
+
+#' @references 
+#' To cite \pkg{hydRology} in publications, use the command \code{citation("hydRology")} to get the current version of the citation.\cr
+
+NULL
