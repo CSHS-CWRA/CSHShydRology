da45ec1b7e79544351a2425cf33ee791cb2b81ba
Kevin Shook <kevin.shook@usask.ca>
2021-06-11 14:08:01 -0600

    renamed ch_get_url_data

--- R/ch_get_url_data.R
+++ R/ch_get_url_data.R
@@ -1,4 +1,4 @@
-#'@title Gets remote data sets
+#' @title Gets remote data sets
 
 #' @description Accesses data sets, via a url the first time,
 #'saves them locally, then accesses them locally after the first time the script is executed.
@@ -6,16 +6,16 @@
 #' @param gd_url url for accessing data set
 #' @param gd_filename name of file on local drive, including full path
 #' 
-#' @author Dan Moore
+#' @author Dan Moore <dan.moore@ubc.ca>
 #'
 #' @importFrom httr GET write_disk
 #' @importFrom sf st_read
 #' @importFrom raster raster
 #'
-#'@return Returns a data frame (from a .csv file), a \code{raster} object (from a .tif file), 
+#' @return Returns a data frame (from a .csv file), a \code{raster} object (from a .tif file), 
 #'or an \code{sf} object (from a GeoJSON file).
 #'
-#'@examples
+#' @examples
 #'# Tested using files in the Upper Penticton Creek
 #'# zenodo repository https://zenodo.org/record/4781469
 #' library(ggplot2)
@@ -31,19 +31,19 @@
 #' # test with soil moisture data in csv format
 #' sm_fn <- here("test_data", "sm_data.csv")
 #' sm_url <- "https://zenodo.org/record/4781469/files/sm_data.csv"
-#' sm_data <- ch_urlgetdata(sm_url, sm_fn)
+#' sm_data <- ch_get_url_data(sm_url, sm_fn)
 #' head(sm_data)
 # 
 #' # test with tif/tiff file containing a dem
 #' ra_fn <- here("test_data", "gs_dem25.tif")
 #' ra_url <- "https://zenodo.org/record/4781469/files/gs_dem25.tif"
-#' ra_data <- ch_urlgetdata(ra_url, ra_fn)
+#' ra_data <- ch_get_url_data(ra_url, ra_fn)
 #' plot(ra_data)
 # 
 #' # test with GeoJSON
 #' gs_fn <- here("test_data", "gs_soilmaps.GeoJSON")
 #' gs_url <- "https://zenodo.org/record/4781469/files/gs_soilmaps.GeoJSON"
-#' gs_data <- ch_urlgetdata(gs_url, gs_fn)
+#' gs_data <- ch_get_url_data(gs_url, gs_fn)
 # 
 #' ggplot(gs_data) +
 #'   geom_sf(aes(fill = new_key)) +
@@ -55,7 +55,7 @@
 #'   
 #' @export
 #' 
-ch_urlgetdata <- function(gd_url, gd_filename) {
+ch_get_url_data <- function(gd_url, gd_filename) {
   file_ext <- strsplit(x = gd_filename, split = "[.]")[[1]][2]
   
   # csv file - returns data frame
@@ -73,11 +73,9 @@ ch_urlgetdata <- function(gd_url, gd_filename) {
   if (file_ext %in% c("tif", "tiff")) {
     if (!file.exists(gd_filename)) {
       GET(gd_url, write_disk(gd_filename))
-      da <- raster(gd_filename)
-    } else {
-      da <- raster(gd_filename)
-    }     
-    return(da) 
+    }
+    da <- raster::raster(gd_filename)
+    return(da)
   }
   
   # GeoJSON - returns sf object
