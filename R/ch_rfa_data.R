87a57f8bde78479bf64bfeae131a662de5e5f956
Kevin Shook <kevin.shook@usask.ca>
2021-10-24 20:47:59 -0600

    fixed a few more typos

--- R/ch_rfa_data.R
+++ R/ch_rfa_data.R
@@ -2,7 +2,7 @@
 #' Streamflow data
 #'
 #' Daily river discharge for the station 01AD002 on
-#' St-John River at Fort Kent, New Brunswick.
+#' St. John River at Fort Kent, New Brunswick.
 #' Data ranges from 1926 to 2014, for basin area of 14700 sq km.
 #' 
 #' @author Martin Durocher
@@ -14,8 +14,8 @@
 #' Annual maxima from sites in the Atlantic region of Canada
 #'
 #' Contains the annual maxima of 45 hydrometric stations found in the
-#' region '01' of Water Survery of Canada.
-#' In additional to the annual maximuma, the output list includes catchment
+#' region '01' of Water Survey of Canada.
+#' In additional to the annual maxima, the output list includes catchment
 #' descriptors (longitude, latitude, basin area, mean annual precipitation)
 #' and the geographical distance between each station.
 #'
