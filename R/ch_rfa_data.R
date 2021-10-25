d56e23c1888670f7028fea2338ee659e7ae31dfd
Kevin Shook <kevin.shook@usask.ca>
2021-10-24 18:04:27 -0600

    removed Martin Derocher's (incorrect) email and fixed a few typos in the comments

--- R/ch_rfa_data.R
+++ R/ch_rfa_data.R
@@ -3,23 +3,23 @@
 #'
 #' Daily river discharge for the station 01AD002 on
 #' St-John River at Fort Kent, New Brunswick.
-#' Data range from 1926 to 2014 and have a drainage area of 14700 sq km.
+#' Data ranges from 1926 to 2014, for basin area of 14700 sq km.
 #' 
-#' @author Martin Durocher <mduroche@@uwaterloo.ca>
+#' @author Martin Durocher
 #' @source \url{https://wateroffice.ec.gc.ca/}
 #####################################################################
 "flowStJohn"
 
 #####################################################################
-#' Annual maximums from sites in the Atlantic region of Canada
+#' Annual maxima from sites in the Atlantic region of Canada
 #'
-#' Contains the annual maximums of 45 hydrometric stations found in the
+#' Contains the annual maxima of 45 hydrometric stations found in the
 #' region '01' of Water Survery of Canada.
-#' Additionaly to the annual maximums, the output list includes catchment
-#' descriptors (longitude, latitude, drainage area, mean annual precipitation)
+#' In additional to the annual maximuma, the output list includes catchment
+#' descriptors (longitude, latitude, basin area, mean annual precipitation)
 #' and the geographical distance between each station.
 #'
-#' @author Martin Durocher <mduroche@@uwaterloo.ca>
+#' @author Martin Durocher
 #' @source \url{https://wateroffice.ec.gc.ca/}
 #####################################################################
 "flowAtlantic"
