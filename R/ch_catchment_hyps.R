f10de1343b2f7318b72914d01a7873d023a50aed
Kevin Shook <kevin.shook@usask.ca>
2021-11-24 08:18:32 -0600

    added ch_catchment_hyps

---
+++ R/ch_catchment_hyps.R
@@ -0,0 +1,72 @@
+#' Catchment hypsometry
+#' @description Finds the hypsometric curve, which is the total fraction of
+#' the area below vs. elevation, for a given basin. The elevations may be
+#' passed as a vector of elevations, or of elevation quantiles, or as minimum
+#' and maximum elevations and the number of elevation intervals. A plot of the 
+#' curve may also be created.
+#' @param catchment A \pkg{sf} object containing the catchment divide.
+#' @param dem A \pkg{raster} object of the Digital Elevation Model.
+#' @param z_levels Vector of elevation levels for the hypsometry. If specified, 
+#' then no other elevation parameters are required. Default is \code{NULL}.
+#' @param n_levels If specified, sets number of elevation intervals. 
+#' Can be used with \code{zmin} and \code{zmax}. Default is \code{NULL}.
+#' @param zmin Minimum elevation for hypsometry. If not specified, minimum
+#' catchment elevation is used. Default is \code{NULL}.
+#' @param zmax Maximum elevation for hypsometry. If not specified, maximum
+#' catchment elevation is used. Default is \code{NULL}.
+#' @param quantiles Vector of elevation quantiles. Default is \code{NULL}.
+#' @param hypso_plot if \code{TRUE} the hypsometric curve is plotted. Default is
+#' \code{NULL}.
+#' @param z_units Elevation units for plot. Default is \option{m}. 
+#' @param col Colour for plot. Default is \option{red}.
+#' @param type Type of plot. Defailt is \option{o} (lines with overplotted
+#' points).
+#' @param xlab Plot x-axis label.
+#' @param ylab Plot y-axis label.
+#' @param add_grid If \code{TRUE}, a grid is added to the plot. Default is 
+#' \code{FALSE}
+#' @param ... Other parameters for the graph
+#'
+#' @importFrom sf as_Spatial
+#' @impportFrom raster mask minValue maxValue quantile
+#' @return Returns a data frame of elevations and catchment fractions below.
+#' @author Dan Moore
+#' @export
+#'
+#' @examples
+ch_catchment_hyps <- function(catchment, dem,
+                          z_levels = NULL, n_levels = 10,
+                          zmin = NULL, zmax = NULL, 
+                          quantiles = NULL,
+                          hypso_plot = FALSE, z_units = "m", 
+                          col = "red", type = "o", 
+                          xlab = "Fraction of catchment below given elevation",
+                          ylab = paste0("Elevation (", z_units, ")"), 
+                          add_grid = FALSE, ...) {
+  # need to add error traps for incorrect values for
+  # catchment and dem
+  catchment_sp <- as_Spatial(catchment)
+  dem_masked <- raster::mask(dem, catchment_sp)
+  if (is.null(quantiles)) {
+    if (is.null(z_levels)) {
+      if (is.null(zmin)) zmin <- raster::minValue(dem_masked)
+      if (is.null(zmax)) zmax <- raster::maxValue(dem_masked)    
+      z_levels <- seq(zmin, zmax, length.out = n_levels)
+    }
+    # there may be a more direct way, but this works
+    z_hist <- raster::hist(dem_masked, plot = FALSE, breaks = z_levels)
+    nz <- sum(z_hist$counts)
+    qz <- c(0, cumsum(z_hist$counts)/nz)
+    out_df <- data.frame(z = z_levels, qz)
+  } else {
+    zq <- raster::quantile(dem_masked, probs = quantiles)
+    out_df <- data.frame(z = zq, qz = quantiles)
+  }
+  if (hypso_plot) {
+    plot(out_df$qz, out_df$z,
+         col = col, xlab = xlab, ylab = ylab, type = type, ...
+    )
+    if (add_grid) grid()
+  }
+  return(out_df)
+}
