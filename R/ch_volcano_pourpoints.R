f8844a78cfe799179456d38cef3dff6d4b24d725
Kevin Shook <kevin.shook@usask.ca>
2022-02-01 19:40:26 -0600

    added examples to ch_wbt_pourpoints

--- R/ch_volcano_pourpoints.R
+++ R/ch_volcano_pourpoints.R
@@ -1,16 +1,22 @@
 #' Creates a sample file of pour points
 #' 
-#' @description Creates a file of pour points for the \code{volcano} DEM
+#' @description Creates a file of pour points for the \code{volcano} DEM. The 
+#' pour points define the outlets of sub-basins. These pour points are used
+#' by examples within other functions.
 #' @param pp_shp Name for shapefile to hold pour points
 #'
-#' @return Returns \code{NULL}. Writes pour points to specified file.
+#' @return Returns an \pkg{sf} object containing 2 pour points for the 
+#' \code{volcano} DEM. The pour points are also written to the specified file.
 #' @export
 #' @importFrom dplyr mutate
 #' @importFrom sf st_as_sf st_set_crs st_write
-#' @author Dan Moore
+#' @author Dan Moore and Kevin Shook
 #' @seealso \code{\link{ch_volcano_raster}} \code{\link{ch_wbt_pourpoints}} 
 #'
 #' @examples
+#' pourpoint_file <- tempfile("volcano_pourpoints", fileext = c(".shp"))
+#' pourpoints <- ch_volcano_pourpoints(pourpoint_file)
+#' plot(pourpoints)
 ch_volcano_pourpoints <- function(pp_shp) {
   
   if(missing(pp_shp)) {
@@ -22,6 +28,9 @@ ch_volcano_pourpoints <- function(pp_shp) {
     mutate(test_label = c("test_1", "test_2")) %>%
     st_as_sf(coords = c("x", "y")) %>%
     st_set_crs(32760)
-  st_write(outlet_sf, pp_shp, delete_dsn = TRUE)
-  return(NULL)
+  if (file.exists(pp_shp))
+    st_write(outlet_sf, pp_shp, delete_dsn = TRUE)
+  else
+    st_write(outlet_sf, pp_shp, delete_dsn = FALSE)
+  return(outlet_sf)
 }
--- R/ch_wbt_pourpoints.R
+++ R/ch_wbt_pourpoints.R
@@ -4,16 +4,18 @@
 #' the pour points to delineate catchments, they must align with the drainage
 #' network. This function snaps (forces the locations) of pour points to the
 #' channels.
-#' @param pp_sf File name containing pour points. These must be supplied by the user.
+#' @param pp_sf \pkg{sf} object containing pour points. These must be supplied by the user. See
+#' the code in \code{\link{ch_volcano_pourpoints}} for an example of creating the object.
 #' @param fn_flowacc Name of file containing flow accumulations.
-#' @param fn_pp 
-#' @param fn_pp_snap 
+#' @param fn_pp File name to create un-snapped pour points.
+#' @param fn_pp_snap File name for snapped pour points.
 #' @param check_crs If \code{TRUE} the projections of the pour points and flow
 #' accumulation files will be checked to ensure they are identical.
-#' @param snap_dist 
+#' @param snap_dist Maximum snap distance in map units. 
 #' @param ... Additional parameters for \pkg{whitebox} function \code{wbt_snap_pour_points}.
 #'
 #' @author Dan Moore
+#' @seealso \code{\link{ch_volcano_pourpoints}}
 #' @importFrom raster raster
 #' @importFrom whitebox wbt_snap_pour_points
 #' @importFrom sf st_crs st_write
@@ -21,12 +23,30 @@
 #' channel network.
 #' @export 
 #' @examples
+#' library(raster)
+#' test_raster <- ch_volcano_raster()
+#' dem_raster_file <- tempfile(fileext = c(".tif"))
+#' no_sink_raster_file <- tempfile("no_sinks", fileext = c(".tif"))
+#' # write test raster to file
+#' writeRaster(test_raster, dem_raster_file, format = "GTiff")
+#' # remove sinks
+#' removed_sinks <- ch_wbt_removesinks(dem_raster_file, no_sink_raster_file, method = "fill")
+#' # get flow accumulations
+#' flow_acc_file <- tempfile("flow_acc", fileext = c(".tif"))
+#' flow_acc <- ch_wbt_flow_accumulation(no_sink_raster_file, flow_acc_file)
+#' # get pour points
+#' pourpoint_file <- tempfile("volcano_pourpoints", fileext = c(".shp"))
+#' pourpoints <- ch_volcano_pourpoints(pourpoint_file)
+#' snapped_pourpoint_file <- tempfile("snapped_pourpoints", fileext = c(".shp"))
+#' snapped_pourpoints <- ch_wbt_pourpoints(pourpoints, flow_acc_file, pourpoint_file,
+#' snapped_pourpoint_file, snap_dist = 10)
+#' 
 ch_wbt_pourpoints <- function(pp_sf = NULL, fn_flowacc, fn_pp, fn_pp_snap, 
                               check_crs = TRUE, snap_dist = NULL, ...) {
   if (!file.exists(fn_flowacc)) {
     stop("Error: flow accumulation file does not exist")
   }
-  if (is.null(pp_sf)) {
+  if (missing(pp_sf)) {
     stop("Error: value for pp_sf missing")
   }
   if (is.null(snap_dist)) {
