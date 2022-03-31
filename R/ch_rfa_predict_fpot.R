cab5e902805b274a5303b3ab6122d11cff68327f
Kevin Shook <kevin.shook@usask.ca>
2022-03-31 11:05:38 -0600

    first addition of pot functions

---
+++ R/ch_rfa_predict_fpot.R
@@ -0,0 +1,198 @@
+###############################################################
+#' Prediction of flood quantiles of a given return period.
+#'
+#' Return a vector or matrix of the flood quantiles, its standard deviation and 
+#' confidence intervals.  
+#' 
+#' @author Martin Durocher <mduroche@@uwaterloo.ca>
+#' 
+#' @param object Output of \link{FitPot}.
+#' 
+#' @param rt Return period. 
+#' 
+#' @param se Logical. Should the standard deviation be returned.
+#' 
+#' @param ci,alpha Method for evaluation the confidence intervals with 
+#'   probability \code{1-alpha}. Available methods are : 
+#'   Delta method (\code{'delta'}), profile likelihood (\code{'profile'}) 
+#'   and nonparametric bootstrap (\code{'boot'})
+#'   
+#' @param nsim Number of bootstrap sample.
+#' 
+#' @param out.matrix Logical. Should the bootstrap sample be returned?
+#' 
+#' @param ... Other parameters.
+#' 
+#' @seealso \link{FitPot}
+#' 
+#' @references 
+#' 
+#' Coles S. (2001) An introduction to statistical modeling of extreme values.
+#'   Springer Verlag.
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' data(flowStJohn)
+#' 
+#' fit <- FitPot(flow~date, flowStJohn, u = 1000,
+#'                declust = 'wrc', r = 14)
+#'      
+#' predict(fit, se = TRUE, ci = 'delta')
+#' 
+predict.fpot <- 
+  function(object, 
+           rt = c(2, 5, 10, 20, 50, 100), 
+           se = FALSE,
+           ci = "none", 
+           alpha = 0.05, 
+           nsim = 1000,
+           out.matrix = FALSE,
+           ...){
+
+  ## funtion that compute the return period
+  Fpred <- function(para, lambda){
+
+    if(abs(para[2]) > sqrt(.Machine$double.eps))
+      ans <- object$u + para[1]/para[2] * (1-lambda^(-para[2]) )
+    else
+      ans <- object$u + para[1] * log(lambda)
+
+    return(ans)
+  }
+
+  lambda <- object$nexcess * rt * object$unit / object$ntot
+  hatRt <- Fpred(object$estimate, lambda)
+
+  ## Standard error by delta method
+  if(se | ci %in% c('delta','profile')){
+    negk <- -object$estimate[2]
+    lk <- lambda^negk
+    ak <- object$estimate[1]/negk
+
+    gx1 <-  object$estimate[1] * (object$ntot/object$nexcess)  * lk
+    gx2 <- (lk-1)/negk
+    gx3 <-  ak * ( lk * log(lambda) - gx2)
+
+    gx <- rbind(gx1, gx2, gx3)
+    hatSe <-  sqrt(diag(t(gx) %*% vcov(object, rate = T) %*% gx))
+  }
+
+  ## Confident interval by delta method
+  if(ci == 'delta'){
+     lb <- hatRt - qnorm(1-alpha/2) * hatSe
+     ub <- hatRt + qnorm(1-alpha/2) * hatSe
+  }
+
+  ## Confident interval by nonparametric bootstrap
+  if(ci == 'boot'){
+
+    ## Resample
+    nexcess <- rpois(nsim,object$nexcess)
+    Fsim <- function(l, p) rgpa(l, p[1], p[2])
+    xboot <- lapply(nexcess, Fsim, object$estimate)
+    
+    ## Define the m-observation return level
+    lambdas <- lapply(nexcess, '*', rt * object$unit / object$ntot) 
+    
+    if(object$method == 'lmom')
+      fn <- fgpaLmom
+    else if(object$method == 'mom')
+      fn <- fgpaMom
+    else if(object$method == 'mle2')
+      fn <- fgpa2d
+    else if (object$method == 'mle')
+      fn <- fgpa1d
+    
+    pboot <- lapply(xboot, fn)
+    qboot <- mapply(Fpred, pboot, lambdas)
+
+    ## Compute interval
+    bootci <- t(apply(qboot, 1, quantile, c(alpha/2, 1-alpha/2)))
+    lb <- bootci[,1]
+    ub <- bootci[,2]
+    
+    pboot <- do.call(rbind, pboot)
+    qboot <- t(qboot)
+    colnames(qboot) <- paste0('Q',rt)
+
+  }
+
+  ## Confident interval by profile likelihood
+  if(ci == 'profile'){
+
+    bnd0 <- c(-.5,1)
+
+    ## Bound of the deviance
+    khi <- qchisq(1-alpha,1)
+    lb <- lb0 <- pmax(1e-8, hatRt - 10 * hatSe)
+    ub <- ub0 <- hatRt + 10 * hatSe
+
+    for(ii in seq_along(rt)){
+
+      ## Deviance function in respect of the return period
+      Dfun <- function(z){
+
+        ## Full likelihood
+        llikFull <- sum(dgpa(object$excess, object$estimate[1],
+                                         object$estimate[2], log = TRUE))
+
+        ## Negative log-likelihood
+        nllik <- function(p){
+
+          num <- z - object$u
+
+          if(abs(p) < 1e-8)
+            denum <- log(lambda[ii])
+          else
+            denum <- -(lambda[ii]^(-p) -1)/p
+
+          return(-sum(dgpa(object$excess, pmax(1e-8,num/denum), p, log = TRUE)))
+
+        }
+
+        ## Compute the profile likelihood
+        suppressWarnings(llik0 <- -optimize(nllik, bnd0)$objective)
+        return(2*(llikFull-llik0))
+
+      }
+
+      ## Lower bound
+      suppressWarnings(lb[ii] <- optimize(function(z) abs(Dfun(z) - khi),
+                                          c(lb0[ii],hatRt[ii]))$minimum)
+
+      ## Upper bound
+      suppressWarnings(ub[ii] <- optimize(function(z) abs(Dfun(z) - khi),
+                                          c(hatRt[ii], ub0[ii]))$minimum)
+
+    } # end for
+
+  } # end if
+
+  ## Does ci was computed
+  ci <- !(ci=='none')
+
+  ## construct the output
+  if(se & !ci)
+    ans <- data.frame(pred = hatRt, se = hatSe)
+  else if(!se & ci)
+    ans <- data.frame(pred = hatRt, lower = lb, upper = ub)
+  else if(se & ci)
+    ans <- data.frame(pred = hatRt, se = hatSe, lower = lb, upper = ub)
+  else
+    ans <- data.frame(pred = hatRt)
+
+
+  ## Format the output
+  if(ncol(ans) == 1){
+    ans <- ans[,1]
+  } else {
+    rownames(ans) <- as.character(rt)
+  }
+  
+  if(out.matrix)
+    ans <- list(pred = ans, para = pboot, qua = qboot)
+
+  return(ans)
+}
---
+++ tests/testthat/test_rfa_FitPot.R
@@ -0,0 +1,90 @@
+######################################################
+## testing FitPot and predict.fpot functions
+## Martin Durocher <mduroche@uwaterloo.ca>
+#######################################################
+
+rm(list = ls())
+
+uu <- (1:10000)/10001
+xd <- qgpa(uu, 1, -.2)
+xd0 <- qgpa(uu, 1, 0)
+
+## Simple fit
+fit <- FitPot(xd, u = 0)
+
+## verify the output
+expect_equal(fit$excess, xd)
+expect_equal(fit$time,1:10000)
+expect_equal(signif(fit$estimate), 
+             c(alpha=1.001330, kappa =-0.198129))
+expect_equal(fit$u,0)
+expect_equal(fit$method, 'mle')
+
+vm <- matrix(c(0.000240263, 0.000119972,
+               0.000119972, 0.000143551),
+             2,2)
+colnames(vm) <- rownames(vm) <- c('alpha','kappa')
+expect_equal(signif(fit$varcov),vm) 
+expect_equal(signif(fit$mrl), 1.24874)
+expect_equal(fit$ntot, 10000)
+expect_equal(fit$nexcess, length(fit$excess))
+
+## verify basic functions
+print(fit)
+expect_equal(vcov(fit), fit$varcov) 
+expect_equal(coef(fit), fit$estimate)             
+expect_equal(signif(AIC(fit)),23993.1)
+
+## Check MLE2 for bounded estimation
+expect_warning(fit <- FitPot(c(xd0, 30:40), u = 5, method = 'mle2'))
+expect_equal(coef(fit)[2], c(kappa = -.5))
+expect_equal(fit$nexcess, length(fit$excess))
+
+## Verify that vcov is not returned
+fit <- FitPot(xd0, u = 5, method = 'mle2', varcov = F)
+expect_true(is.null(fit$varcov))
+expect_true(is.null(vcov(fit)))
+
+## try Lmoment estimator
+fit <- FitPot(xd0, u = 5, method = 'lmom', varcov = F)
+fit <- FitPot(xd0, u = 5, method = 'lmom', varcov = T, nsim = 5)
+
+## try moment estimator
+fit <- FitPot(xd0, u = 5, method = 'mom', varcov = F)
+fit <- FitPot(xd0, u = 5, method = 'mom', varcov = T, nsim = 5)
+
+## Verif confidence interval for parameter by profile likelihood
+cc <- coef(fit, ci = TRUE)
+expect_equal(colnames(cc), c('estimate','lower','upper'))
+expect_equal(rownames(cc), c('alpha','kappa'))
+
+expect_equivalent(cc[,1], fit$estimate)
+expect_true(all(cc[,2] < cc[,1]))
+expect_true(all(cc[,3] > cc[,1]))
+
+## verify unit
+expect_equal(fit$unit, 365.25)
+
+fit <- FitPot(xd0, u = 5, method = 'lmom', varcov = F, unit = 100)
+expect_equal(fit$nyear, 100)
+
+
+## verify the declustering
+
+fit <- FitPot(flow~date, flowStJohn, u = 1000,
+              declust = 'wrc', r = 10, rlow = .6)
+xid <- which.floodPeaks(flow~date, flowStJohn, u = 1000, r = 10, rlow = .6)
+expect_equal(flowStJohn$flow[xid], fit$excess + fit$u)
+
+fit <- FitPot(flow~date, flowStJohn, u = 900, declust = 'run', r = 10)
+xid <- which.clusters(flow~date, flowStJohn, u = 900, r = 10)
+expect_equal(flowStJohn$flow[xid], fit$excess + fit$u)
+
+## verify formula
+fit1 <- FitPot(flow~date, flowStJohn, u = 1000,
+              declust = 'wrc', r = 10, rlow = .6)
+
+fit2 <- FitPot(flowStJohn$flow, flowStJohn$date, u = 1000,
+              declust = 'wrc', r = 10, rlow = .6)
+
+expect_equal(fit1,fit2)
