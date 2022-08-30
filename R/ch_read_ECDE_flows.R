69d6e87ae18fa00bfabf18d62e470dcb1634d58d
Kevin Shook <kevin.shook@usask.ca>
2022-08-30 08:49:27 -0600

    updated HYDAT_list.RData ch_read_ECDE_flows now reads from url

--- R/ch_read_ECDE_flows.R
+++ R/ch_read_ECDE_flows.R
@@ -1,7 +1,8 @@
 #' Reads a file of WSC daily flows from ECDataExplorer (ECDE)
 #'
 #' @description Reads in a file WSC daily flows as returned from the Windows program ECDataExplorer, converts the Date,
-#' and omits the last 3 lines as these contain the data disclaimer and not data.
+#' and omits the last 3 lines as these contain the data disclaimer and not data. The function
+#' can read values from a url.
 #'
 #' @param filename Datafile retrieved from ECDataExplorer.
 #'
@@ -23,6 +24,12 @@
 #' mfile <- "04JD005_Daily_Flow_ts.csv"
 #' mdata <- ch_read_ECDE_flows(mfile)}
 #' 
+#' \donttest{
+#' # Not tested automatically as it is slow to read from a url
+#' url1 <- "https://zenodo.org/record/7007830/files/08NL007_Daily_Flow_ts.csv?"
+#' values <- ch_read_ECDE_flows(url1)
+#' }
+#' 
 ch_read_ECDE_flows <- function(filename) {
   
   # check ECDE filename
@@ -32,7 +39,8 @@ ch_read_ECDE_flows <- function(filename) {
   
   if (!file.exists(filename)) {
     # check if actually a url
-    if (!ch_test_url_file(filename))
+    result <- ch_test_url_file(filename, quiet = TRUE)
+    if (result != "OK")
       stop("ECDE file not found")
   }
   mdata <- read.csv(filename, header = FALSE, skip = 1)
@@ -40,8 +48,6 @@ ch_read_ECDE_flows <- function(filename) {
   mdata$Date <- as.Date(mdata$Date, format = "%Y/%m/%d")
   cut <- length(mdata[, 1]) - 3
   mdata <- mdata[1:cut, ]
-  # change "Flow,m3.c"   to "Flow" to deal with the difference in ECDE versions
-  names(mdata)[4] <- substr(names(mdata)[4],1,4)
 
   if (names(mdata)[4] != "Flow") 
     message("Input data file format error")
