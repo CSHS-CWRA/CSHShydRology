1e6fa30873aef768c1da6c8c3a929623282325d9
Kevin Shook <kevin.shook@usask.ca>
2024-07-12 00:09:25 -0600

    fixed errors in some of Paul's functions

--- R/Floodnet_functions.R
+++ R/Floodnet_functions.R
@@ -1,9 +1,9 @@
-# Floodnet functions
-#' @name Floodnet-functions
+#' Floodnet functions
+#' @name Floodnet_functions
 #' @description These functions were written by Martin Durocher as part of the 
 #' FloodNet program \url{https://www.nsercfloodnet.ca/}.
 #' The functions perform flood-frequency analyses, so they are all prefixed
-#' with "ch_rfa_", so that they can be identified.  
+#' with \code{ch_rfa_}, so that they can be identified.  
 #' 
 #' Currently, only a few of the functions have been added to \pkg{CSHShydRology}. 
 #' We are adding more as time permits.
